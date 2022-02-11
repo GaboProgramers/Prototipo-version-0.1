d5b4f90917005884dbf2f10e193b7fa8deb3d070
leonardo <leonardo2322@gmail.com>
2022-02-11 12:26:40 -0500

    nuevos cambios

--- js/script.js
+++ js/script.js
@@ -24,3 +24,31 @@ window.onscroll = () =>{
    menuBtn.classList.remove('fa-times');
    navbar.classList.remove('active');
 }
+
+// Funcion Para Iconos del Carrito//
+
+function mostrarResultado() {
+   document.getElementById('resultado').style.display = 'block'
+}
+
+function ocultarP() {
+   document.getElementById('resultado').style.display = 'none'
+}
+
+window.onscroll = function(){
+   if(document.documentElement.scrollTop > 50) {
+      document.querySelector('.go-top-container')
+      .classList.add('show');
+   } else {
+      document.querySelector('.go-top-container')
+      .classList.remove('show');
+   }
+}
+
+document.querySelector('.go-top-container')
+.addEventListener('click', () => {
+   window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+   })
+});
